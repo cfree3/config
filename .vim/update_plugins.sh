fac6d4444a6c2947704d030e0ced44d1b95518df
Curtis Free <cf@curtisfree.com>
2016-05-10 09:37:24 -0400

    Update to latest versions of Vim plugins

--- .vim/update_plugins.sh
+++ .vim/update_plugins.sh
@@ -2,7 +2,10 @@
 # update_plugins.sh | Curtis Free (http://curtisfree.com)
 # Updates all Vim plugins to the latest versions.
 
-cd bundle
-for plugin in *; do
-    ( cd ${plugin} && git pull --rebase ) && git add ${plugin}
-done
+# assume all submodules are Vim plugins so that `foreach` is a handy shortcut
+
+# make sure all plugins are ready (http://stackoverflow.com/a/4438292)
+git submodule update --init --recursive
+
+# bring everything up to master
+git submodule foreach 'git fetch origin master && git reset --hard FETCH_HEAD'
