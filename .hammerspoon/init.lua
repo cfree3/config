b70eb63412b2b462ab271f39d87c95221fca89cd
Curtis Free <cf@curtisfree.com>
2021-11-02 22:17:09 -0400

    Clean up and add to Hammerspoon config
    The "grid" feature is fantastic! I also added shortcuts that directly
    allow me to split a screen into side-by-side thirds.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -28,6 +28,19 @@ bind_threekey = { "ctrl", "alt", "cmd" }
 --
 
 --
+--  ( 2 key ) + '
+--
+--    Interactive window resizing and placement.
+--
+hs.grid.setGrid({ w=6, h=4 })
+hs.grid.setMargins({ w=0, h=0 })
+hs.grid.ui.textSize = 40
+hs.grid.ui.showExtraKeys = false
+hs.hotkey.bind(bind_twokey, "'", function()
+    hs.grid.show()
+end)
+
+--
 --  ( 2 key ) + h
 --
 --    Move window to left half of the screen.
@@ -116,14 +129,78 @@ hs.hotkey.bind(bind_twokey, "l", function()
 end)
 
 --
--- ( 2 key ) + f (because I'm accustomed to it from my Slate config)
---  - OR -
--- ( 2 key ) + ; (easy)
+--  ( 2 key ) + b
 --
---   Make window take up the entire screen (not to be confused with macOS's
---   fullscreen).
+--    Move window to left third of the screen.
 --
-function windowFullScreen()
+hs.hotkey.bind(bind_twokey, "b", function()
+
+  local window = hs.window.focusedWindow()
+  local windowFrame = window:frame()
+
+  local screen = window:screen()
+  local screenFrame = screen:frame()
+
+  windowFrame.x = screenFrame.x
+  windowFrame.y = screenFrame.y
+  windowFrame.w = screenFrame.w / 3
+  windowFrame.h = screenFrame.h
+
+  window:setFrame(windowFrame)
+
+end)
+
+--
+--  ( 2 key ) + n
+--
+--    Move window to middle third of the screen.
+--
+hs.hotkey.bind(bind_twokey, "n", function()
+
+  local window = hs.window.focusedWindow()
+  local windowFrame = window:frame()
+
+  local screen = window:screen()
+  local screenFrame = screen:frame()
+
+  windowFrame.x = screenFrame.x + ( screenFrame.w / 3 )
+  windowFrame.y = screenFrame.y
+  windowFrame.w = screenFrame.w / 3
+  windowFrame.h = screenFrame.h
+
+  window:setFrame(windowFrame)
+
+end)
+
+--
+--  ( 2 key ) + m
+--
+--    Move window to right third of the screen.
+--
+hs.hotkey.bind(bind_twokey, "m", function()
+
+  local window = hs.window.focusedWindow()
+  local windowFrame = window:frame()
+
+  local screen = window:screen()
+  local screenFrame = screen:frame()
+
+  windowFrame.x = screenFrame.x + 2 * ( screenFrame.w / 3 )
+  windowFrame.y = screenFrame.y
+  windowFrame.w = screenFrame.w / 3
+  windowFrame.h = screenFrame.h
+
+  window:setFrame(windowFrame)
+
+end)
+
+--
+-- ( 2 key ) + ;
+--
+--   Make window take up the entire screen.
+--   (NOT macOS's fullscreen takeover.)
+--
+hs.hotkey.bind(bind_twokey, ";", function()
 
   local window = hs.window.focusedWindow()
   local windowFrame = window:frame()
@@ -138,9 +215,7 @@ function windowFullScreen()
 
   window:setFrame(windowFrame)
 
-end
-hs.hotkey.bind(bind_twokey, ";", windowFullScreen)
-hs.hotkey.bind(bind_twokey, "f", windowFullScreen)
+end)
 
 --
 -- ( 3 key ) + h
@@ -148,11 +223,8 @@ hs.hotkey.bind(bind_twokey, "f", windowFullScreen)
 --   Move window one screen (monitor) to the left.
 --
 hs.hotkey.bind(bind_threekey, "h", function()
-
     local window = hs.window.focusedWindow()
-
     window:moveOneScreenWest()
-
 end)
 
 --
@@ -161,11 +233,8 @@ end)
 --   Move window one screen (monitor) below.
 --
 hs.hotkey.bind(bind_threekey, "j", function()
-
     local window = hs.window.focusedWindow()
-
     window:moveOneScreenSouth()
-
 end)
 
 --
@@ -174,11 +243,8 @@ end)
 --   Move window one screen (monitor) above.
 --
 hs.hotkey.bind(bind_threekey, "k", function()
-
     local window = hs.window.focusedWindow()
-
     window:moveOneScreenNorth()
-
 end)
 
 --
@@ -187,11 +253,8 @@ end)
 --   Move window one screen (monitor) to the right.
 --
 hs.hotkey.bind(bind_threekey, "l", function()
-
     local window = hs.window.focusedWindow()
-
     window:moveOneScreenEast()
-
 end)
 
 --
@@ -201,15 +264,4 @@ end)
 --
 hs.hotkey.bind(bind_onekey, "Escape", function()
     hs.caffeinate.startScreensaver()
-end)
-
---
--- ( 1 key ) + ;
---
---   Show a list of windows with hints for quick selection.
---
-hs.hotkey.bind(bind_onekey, ";", function()
-    hs.hints.style = "vimperator"
-    hs.hints.fontSize = 13.0
-    hs.hints.windowHints()
 end)
