0f3b43b0306ab85dcbaf668b949f8ad620b6a69d
Curtis Free <cf@curtisfree.com>
2025-11-02 14:43:10 -0500

    Improve coloring (highlight) for Telescope

--- nvim/.config/nvim/after/ftplugin/telescopeprompt.lua
+++ nvim/.config/nvim/after/ftplugin/telescopeprompt.lua
@@ -4,9 +4,13 @@
 -- Colors & Style
 
 vim.api.nvim_set_hl(0, "TelescopeBorder", { link = "Directory" })
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { link = "Normal" })
 vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { link = "ModeMsg" })
 vim.api.nvim_set_hl(0, "TelescopePromptBorder", { link = "ModeMsg" })
 vim.api.nvim_set_hl(0, "TelescopePromptNormal", { link = "Normal" })
-vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { link = "ModeMsg" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { link = "LineNr" })
 vim.api.nvim_set_hl(0, "TelescopePromptTitle", { link = "ModeMsg" })
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { link = "Normal" })
 vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { link = "Normal" })
+vim.api.nvim_set_hl(0, "TelescopeSelection", { link = "CursorLine" })
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { link = "CursorLineNr" })
