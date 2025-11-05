97ca607be681c4887076cdd1b23f9a7e71385000
Curtis Free <cf@curtisfree.com>
2025-11-04 21:45:40 -0500

    Tweak/improve usability of Telescope
    Includes leveraging "native" FZF for Telescope queries.

--- nvim/.config/nvim/lua/plugins/spec/telescope.lua
+++ nvim/.config/nvim/lua/plugins/spec/telescope.lua
@@ -4,16 +4,25 @@ return {
   "nvim-telescope/telescope.nvim",
   name = "telescope",
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    name = "plenary",
+    {
+      "nvim-lua/plenary.nvim",
+      name = "plenary",
+    },
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      name = "telescope-fzf",
+      build = "make",
+    },
   },
-  init = function()
-    -- keymaps (when to use Telescope)
+  config = function()
+    -- Keymaps (when to use Telescope)
     vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
     vim.keymap.set("n", "<C-A-p>", ":Telescope live_grep<CR>")
+    -- ▶︎ See https://vonheikemen.github.io/devlog/tools/using-netrw-vim-builtin-file-explorer/
+    vim.keymap.set("n", "<C-S-p>", ':lua require("telescope.builtin").resume()<CR>')
     vim.keymap.set("n", ";", ":Telescope buffers<CR>")
 
-    -- Telescope settings
+    -- Settings
     require("telescope").setup({
       defaults = {
         mappings = {
@@ -53,5 +62,10 @@ return {
         },
       },
     })
+
+    -- Extensions
+    -- ▶︎ Use native FZF to find files by name (more capable than builtin)
+    --   ▶︎ See https://www.reddit.com/r/neovim/comments/1fk4r04/comment/lnt77pj/
+    require("telescope").load_extension("fzf")
   end,
 }
