82464745d3e479b458db3325a794304282c411fb
Curtis Free <cf@curtisfree.com>
2025-11-07 22:03:37 -0500

    [nvim] Update Markdown headers for visibility
    Matching sufficiently-appropriate colors from the Tomorrow and Tomorrow
    Night colorschemes.

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -4,19 +4,20 @@
 -- Keep lines <= 100 chars
 vim.opt_local.textwidth = 100
 vim.opt_local.colorcolumn = "101"
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = "DarkYellow", bold = true })
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = vim.g.terminal_color_8 })
 
 -- Colors & Highlighting
 
 vim.treesitter.start()
 
--- ▶︎ ROY G BV (or as close as I could get!)
-vim.api.nvim_set_hl(0, "Header1", { fg = "Red", bold = true })
-vim.api.nvim_set_hl(0, "Header2", { fg = "DarkYellow", bold = true })
-vim.api.nvim_set_hl(0, "Header3", { fg = "Yellow", bold = true })
-vim.api.nvim_set_hl(0, "Header4", { fg = "Green", bold = true })
-vim.api.nvim_set_hl(0, "Header5", { fg = "Blue", bold = true })
-vim.api.nvim_set_hl(0, "Header6", { fg = "DarkMagenta", bold = true })
+-- ▶︎ ROY G BV... (or as close as I could get!)
+--   ▶︎ Based on the "terminal colors" as set by base16-tomorrow{,-night}
+vim.api.nvim_set_hl(0, "Header1", { fg = vim.g.terminal_color_1, bold = true })
+vim.api.nvim_set_hl(0, "Header2", { fg = vim.g.terminal_color_3, bold = true })
+vim.api.nvim_set_hl(0, "Header3", { fg = vim.g.terminal_color_2, bold = true })
+vim.api.nvim_set_hl(0, "Header4", { fg = vim.g.terminal_color_6, bold = true })
+vim.api.nvim_set_hl(0, "Header5", { fg = vim.g.terminal_color_4, bold = true })
+vim.api.nvim_set_hl(0, "Header6", { fg = vim.g.terminal_color_5, bold = true })
 
 -- ▶︎ Apply to markdown headers in Treesitter
 vim.api.nvim_set_hl(0, "@markup.heading.1.markdown", { link = "Header1" })
