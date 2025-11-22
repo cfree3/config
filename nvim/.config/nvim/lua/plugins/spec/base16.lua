f14e34b9b7afb62e763aa0b2c3b063da143ea384
Curtis Free <cf@curtisfree.com>
2025-11-22 10:35:11 -0500

    [nvim] Update dark mode detection
    Addresses Lua LSP finding for nil checks and also reworks the logic. One
    may argue whether or not the new implementation is simpler....

--- nvim/.config/nvim/lua/plugins/spec/base16.lua
+++ nvim/.config/nvim/lua/plugins/spec/base16.lua
@@ -8,26 +8,26 @@ return {
     vim.g.base16colorspace = 256
 
     -- Set colorscheme based on system mode (dark/light)
-    do
-      local color_dark = "base16-tomorrow-night"
-      local color_light = "base16-tomorrow"
+    local colorscheme_dark = "base16-tomorrow-night"
+    local colorscheme_light = "base16-tomorrow"
 
-      if vim.fn.has("mac") == 1 then
-        -- https://www.reddit.com/r/neovim/comments/y2by27/comment/is24btb/
-        -- https://github.com/neovim/neovim/issues/21376
-        local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
-        local mode = vim.fn.trim(handle:read("*a"))
-        handle:close()
-        if mode == "Dark" then
-          vim.cmd.colorscheme(color_dark)
-        else
-          vim.cmd.colorscheme(color_light)
-        end
+    -- Default to dark mode
+    local dark_mode = true
 
-      -- (Default to preferred dark theme if we aren't on macOS)
+    -- Attempt to query macOS color mode (light/dark)
+    if vim.fn.has("mac") == 1 then
+      -- https://www.reddit.com/r/neovim/comments/y2by27/comment/is24btb/
+      -- https://github.com/neovim/neovim/issues/21376
+      local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+      if handle == nil then
+        error("Unable to read macOS color mode (light/dark)")
       else
-        vim.cmd.colorscheme(color_dark)
+        dark_mode = vim.fn.trim(handle:read("*a")) == "Dark"
+        handle:close()
       end
     end
+
+    -- And set the theme!
+    vim.cmd.colorscheme(dark_mode and colorscheme_dark or colorscheme_light)
   end,
 }
