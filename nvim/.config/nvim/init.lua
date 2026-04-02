fcd7ec3c88ace670fac08bc01d0385b736820d88
Curtis Free <cf@curtisfree.com>
2026-04-02 19:16:46 -0400

    [nvim] Adjust display of LSP diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -52,7 +52,6 @@ vim.opt.titlestring = [[%t%( (%{expand("%:~:h")})%)%a]]
 -- ▶︎ Gutter
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "number"
 
 -- ▶︎ Status line
 --   ▶︎ See https://curtisfree.com/blog/2012/04/22/adding_total_line_count_to_vim_statusline
--- nvim/.config/nvim/lua/plugins/spec/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/spec/lspconfig.lua
@@ -4,16 +4,37 @@ return {
   "neovim/nvim-lspconfig",
   name = "lspconfig",
   init = function()
-    -- Track which LSPs to use (language = "executable")
+    -- Track which LSPs to use
     local lsps = {
-      go = "gopls",
-      lua = "lua_ls",
+      go = {
+        exe = "gopls",
+        pattern = "*.go",
+      },
+      lua = {
+        exe = "lua_ls",
+        pattern = "*.lua",
+      },
     }
     -- Enable those LSPs iff they are installed
-    for _, exe in pairs(lsps) do
-      if vim.fn.executable(exe) then
-        vim.lsp.enable(exe)
+    for _, lang in pairs(lsps) do
+      if vim.fn.executable(lang.exe) then
+        vim.lsp.enable(lang.exe)
+        -- Show the sign column in files of this type
+        -- (easier to read than signcolumn = "number")
+        vim.api.nvim_create_autocmd("BufEnter", {
+          pattern = lang.pattern,
+          callback = function()
+            vim.opt_local.signcolumn = "yes"
+          end,
+        })
       end
     end
+    -- Configure how Neovim should surface diagnostics
+    vim.diagnostic.config({
+      virtual_text = {
+        virt_text_pos = "eol",
+        virt_text_win_col = 82,
+      },
+    })
   end,
 }
