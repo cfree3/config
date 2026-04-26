5521d3f13ad9d38d2a8200de5d607f34cff86d13
Curtis Free <cf@curtisfree.com>
2026-04-26 10:27:45 -0400

    [nvim] Fix command to set title for Netrw pane
    Also insert additional return to clear command from command line.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -155,7 +155,7 @@ vim.g.netrw_winsize = 20
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_list_hide = "^..*.sw.$"
-vim.keymap.set("n", "-", ':Lexplore<CR>:setlocal winbar=%{expand("%:~:h")}<CR>')
+vim.keymap.set("n", "-", [[:Lexplore<CR>:setlocal winbar=%{expand(\"%:~:h\")}<CR>:<CR>]])
 -- }}}}
 
 -- Clipboard {{{
