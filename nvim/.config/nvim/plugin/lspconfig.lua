72175ce35e1a152a82d3f6cfd08b6265c8399e87
Curtis Free <cf@curtisfree.com>
2026-08-09 08:28:59 -0400

    [nvim] Fix LSP enablement for Lua
    Remove assumption that the configuration and the language server
    executable are identical. In the case of Lua, the nvim-lspconfig plugin
    configures support for `lua_ls`, but the executable is actually
    `lsp-language-server`.
    
    (Perhaps this changed in my installation recently?)

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -14,14 +14,17 @@ do
   -- Track which LSPs to use
   local lsps = {
     go = {
+      name = "gopls",
       exe = "gopls",
       pattern = "*.go",
     },
     lua = {
-      exe = "lua_ls",
+      name = "lua_ls",
+      exe = "lua-language-server",
       pattern = "*.lua",
     },
     python = {
+      name = "pylsp",
       exe = "pylsp",
       pattern = "*.py",
     },
@@ -30,7 +33,7 @@ do
   -- Enable those LSPs iff they are installed
   for _, lang in pairs(lsps) do
     if vim.fn.executable(lang.exe) == 1 then
-      vim.lsp.enable(lang.exe)
+      vim.lsp.enable(lang.name)
       -- Show the sign column in files of this type
       -- (easier to read than signcolumn = "number")
       vim.api.nvim_create_autocmd("BufEnter", {
