255a5098a026d0af2847d702d6598de71d1382e5
Curtis Free <cf@curtisfree.com>
2026-09-13 20:35:30 -0400

    [nvim] Improve syntax highlighting for Vimwiki (Markdown) files

--- nvim/.config/nvim/after/ftplugin/vimwiki.lua
+++ nvim/.config/nvim/after/ftplugin/vimwiki.lua
@@ -1,10 +1,16 @@
 -- ~/.config/nvim/after/ftplugin/vimwiki.lua | Curtis Free (https://curtisfree.com)
 -- Neovim settings specific to VimWiki files.
 
--- Easy movement between days (overrides usual buffer keymaps)
+-- Keymaps
+
+-- ▶︎ Easy movement between days (overrides usual buffer keymaps)
+--   ▶︎ Not on a diary entry? Prev goes to latest entry, and Next does nothing
 vim.keymap.set("n", "<C-h>", ":VimwikiDiaryPrevDay<CR>", { buffer = true })
 vim.keymap.set("i", "<C-h>", "<C-o>:VimwikiDiaryPrevDay<CR>", { buffer = true })
 vim.keymap.set("n", "<C-l>", ":VimwikiDiaryNextDay<CR>", { buffer = true })
 vim.keymap.set("i", "<C-l>", "<C-o>:VimwikiDiaryNextDay<CR>", { buffer = true })
 
--- (Not on a diary entry? Prev goes to latest entry, and Next does nothing)
+-- Colors & Highlighting
+
+-- ▶︎ Analyze file as Markdown (brings in nice rainbow headings!)
+vim.treesitter.start(nil, "markdown")
